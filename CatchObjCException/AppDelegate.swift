0e5da1747c4e40cfd0ba7c5b5c5871b9db470582
Mason Mark <mason@fivespeed.com>
2015-11-04 06:05:07 +0900

    Reformat auto-generated code to remove Wrong Brace Style.

--- CatchObjCException/AppDelegate.swift
+++ CatchObjCException/AppDelegate.swift
@@ -1,27 +1,23 @@
-//
-//  AppDelegate.swift
-//  CatchObjCException
-//
-//  Created by mason on 2015-11-01.
-//  Copyright © 2015 mason. All rights reserved.
-//
+// AppDelegate.swift Created by mason on 2015-11-01. Copyright © 2015 mason. All rights reserved.
 
 import Cocoa
 
 @NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate {
+
+class AppDelegate: NSObject, NSApplicationDelegate
+{
 
   @IBOutlet weak var window: NSWindow!
 
 
-  func applicationDidFinishLaunching(aNotification: NSNotification) {
+  func applicationDidFinishLaunching(aNotification: NSNotification)
+  {
     // Insert code here to initialize your application
   }
 
-  func applicationWillTerminate(aNotification: NSNotification) {
+  func applicationWillTerminate(aNotification: NSNotification)
+  {
     // Insert code here to tear down your application
   }
 
-
 }
-
